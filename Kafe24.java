c3b1867410faf3eb66aa6eb10b54f6dea84ca740
Wahdaapf <wahdaadella.ba@gmail.com>
2024-11-22 13:43:02 +0100

    feat: kafe java

---
+++ Kafe24.java
@@ -0,0 +1,18 @@
+public class Kafe24 {
+
+    public static void Menu() {
+        System.out.println("=====MENU RESTO KAFE=====");
+        System.out.println("1. Kopi Hitam\t - Rp.15.000");
+        System.out.println("2. Cappuccino\t - Rp.20.000");
+        System.out.println("3. Latte\t\t - Rp.22.000");
+        System.out.println("4. Teh Tarik\t - Rp.12.000");
+        System.out.println("5. Roti Bakar\t - Rp.10.000");
+        System.out.println("6. Mie Goreng\t - Rp.18.000");
+        System.out.println("=========================");
+        System.out.println("Silahkan pilih menu yang anda inginkan.");
+    }
+
+    public static void main(String[] args) {
+        Menu();
+    }
+}
